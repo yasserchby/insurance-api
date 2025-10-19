2e01fe48882cb2f3cf0fc26d190b30c05a4d16b9
Yasser <yasser.chby@gmail.com>
2025-10-19 15:41:06 +0200

    Project init

---
+++ src/main/java/com/insurance/insurance_api/InsuranceApiApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.insurance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceApiApplication.class, args);
+	}
+
+}
